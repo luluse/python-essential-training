efee9557ff350c044ffabcd0797d89521110381a
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-06-15 12:21:16 -0700

    exercise ch5

---
+++ Chap05/bitwise.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+
+# ^ Xor only if one or the other has it
+# << Shift left
+# >> Shift right
+
+x = 0x0a
+y = 0x02
+z = x & y
+
+print(f'(hex) x is {x:02x}, y is {y:02x}, z is {z:02x}')
+print(f'(bin) x is {x:08b}, y is {y:08b}, z is {z:08b}')

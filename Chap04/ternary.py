5b2d1f03d1e719f879d9b575dbf1114341539339
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-06-15 11:57:56 -0700

    exercise ch4

---
+++ Chap04/ternary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+
+# 0 or False is the same thing
+hungry = 0
+x = 'Feed the bear now!' if hungry else 'Do not feed the bear.'
+print(x)
